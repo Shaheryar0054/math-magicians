50e538f8188cdcf64b12954f1c614068f721a66a
shaheryar abid <sharyar0310@gmail.com>
2023-02-22 22:31:24 +0500

    Added the calculator UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,37 @@
+import './calculator.css';
+/* eslint-disable import/no-extraneous-dependencies */
+import PropTypes from 'prop-types';
+
+function Calculator(props) {
+  const { onClick } = props;
+
+  return (
+    <div className="container">
+      <button type="button" onClick={onClick} className="btn">AC</button>
+      <button type="button" onClick={onClick} className="btn">+/-</button>
+      <button type="button" onClick={onClick} className="btn">%</button>
+      <button type="button" onClick={onClick} className="btn divide">÷</button>
+      <button type="button" onClick={onClick} className="btn">7</button>
+      <button type="button" onClick={onClick} className="btn">8</button>
+      <button type="button" onClick={onClick} className="btn">9</button>
+      <button type="button" onClick={onClick} className="btn multiply">×</button>
+      <button type="button" onClick={onClick} className="btn">4</button>
+      <button type="button" onClick={onClick} className="btn">5</button>
+      <button type="button" onClick={onClick} className="btn">6</button>
+      <button type="button" onClick={onClick} className="btn minus">-</button>
+      <button type="button" onClick={onClick} className="btn">1</button>
+      <button type="button" onClick={onClick} className="btn">2</button>
+      <button type="button" onClick={onClick} className="btn">3</button>
+      <button type="button" onClick={onClick} className="btn plus">+</button>
+      <button type="button" onClick={onClick} className="btn zero">0</button>
+      <button type="button" onClick={onClick} className="btn">.</button>
+      <button type="button" onClick={onClick} className="btn equal">=</button>
+    </div>
+  );
+}
+
+Calculator.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Calculator;
