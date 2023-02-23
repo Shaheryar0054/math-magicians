0190a56ad23366c6bf4f4bf408b75b289675f5e0
shaheryar abid <sharyar0310@gmail.com>
2023-02-23 12:19:57 +0500

    Modify my code to meet the requirments

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <input value={0} className="display" />
-      </div>
-      <Calculator />
-    </div>
+
+    <Calculator />
+
   );
 }
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,32 +1,34 @@
 import './calculator.css';
-/* eslint-disable import/no-extraneous-dependencies */
 import PropTypes from 'prop-types';
 
 function Calculator(props) {
   const { onClick } = props;
 
   return (
-    <div className="container">
-      <button type="button" onClick={onClick} className="btn">AC</button>
-      <button type="button" onClick={onClick} className="btn">+/-</button>
-      <button type="button" onClick={onClick} className="btn">%</button>
-      <button type="button" onClick={onClick} className="btn divide">÷</button>
-      <button type="button" onClick={onClick} className="btn">7</button>
-      <button type="button" onClick={onClick} className="btn">8</button>
-      <button type="button" onClick={onClick} className="btn">9</button>
-      <button type="button" onClick={onClick} className="btn multiply">×</button>
-      <button type="button" onClick={onClick} className="btn">4</button>
-      <button type="button" onClick={onClick} className="btn">5</button>
-      <button type="button" onClick={onClick} className="btn">6</button>
-      <button type="button" onClick={onClick} className="btn minus">-</button>
-      <button type="button" onClick={onClick} className="btn">1</button>
-      <button type="button" onClick={onClick} className="btn">2</button>
-      <button type="button" onClick={onClick} className="btn">3</button>
-      <button type="button" onClick={onClick} className="btn plus">+</button>
-      <button type="button" onClick={onClick} className="btn zero">0</button>
-      <button type="button" onClick={onClick} className="btn">.</button>
-      <button type="button" onClick={onClick} className="btn equal">=</button>
-    </div>
+    <section className="calculator">
+      <div className="container">
+        <input value={0} className="display" />
+        <button type="button" onClick={onClick} className="btn">AC</button>
+        <button type="button" onClick={onClick} className="btn">+/-</button>
+        <button type="button" onClick={onClick} className="btn">%</button>
+        <button type="button" onClick={onClick} className="btn divide">÷</button>
+        <button type="button" onClick={onClick} className="btn">7</button>
+        <button type="button" onClick={onClick} className="btn">8</button>
+        <button type="button" onClick={onClick} className="btn">9</button>
+        <button type="button" onClick={onClick} className="btn multiply">×</button>
+        <button type="button" onClick={onClick} className="btn">4</button>
+        <button type="button" onClick={onClick} className="btn">5</button>
+        <button type="button" onClick={onClick} className="btn">6</button>
+        <button type="button" onClick={onClick} className="btn minus">-</button>
+        <button type="button" onClick={onClick} className="btn">1</button>
+        <button type="button" onClick={onClick} className="btn">2</button>
+        <button type="button" onClick={onClick} className="btn">3</button>
+        <button type="button" onClick={onClick} className="btn plus">+</button>
+        <button type="button" onClick={onClick} className="btn zero">0</button>
+        <button type="button" onClick={onClick} className="btn">.</button>
+        <button type="button" onClick={onClick} className="btn equal">=</button>
+      </div>
+    </section>
   );
 }
 
