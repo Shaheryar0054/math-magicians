6bc7b8054ebaaa8d85189d4b23f4cc2a565a9b32
shaheryar abid <sharyar0310@gmail.com>
2023-03-01 11:40:40 +0500

    Fix the errors

--- src/components/Rout.js
+++ src/components/Rout.js
@@ -3,7 +3,9 @@ import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
 import './Rout.css';
-import Calculator from './Calculator';
+import Home from './Home';
+import CalculatorPage from './CalculatorPage';
+import Quote from './Quote';
 
 export default function Rout() {
   return (
@@ -34,40 +36,5 @@ export default function Rout() {
         </Route>
       </Switch>
     </Router>
-  );
-}
-
-function Home() {
-  const text = 'Welcome to Math Magicians, your one-stop destination for all things math!\n\n'
-    + 'Whether you\'re a student, a teacher, or just someone who loves math, '
-    + 'we\'ve got everything you need to enhance your math skills and knowledge. '
-    + 'From basic arithmetic to advanced calculus, we\'ve got it all covered. '
-    + 'So why wait? Start exploring our site today and take your math skills to '
-    + 'the next level!';
-  return (
-    <div className="Home-content">
-      <h1>Math Magicians</h1>
-      <h2>Welcome to our page!</h2>
-      <p>{text}</p>
-    </div>
-  );
-}
-function CalculatorPage() {
-  return (
-    <div className="calculator-content">
-      <h1>Math Magicians</h1>
-      <Calculator />
-    </div>
-  );
-}
-function Quote() {
-  return (
-    <div className="Quote-content">
-      <h1>Math Magicians</h1>
-      <h3 className="quote-txt">
-        Mathematics is not about numbers, equtaion, computaion,
-        or algorithms:it is about understanding. -William Paul Thurston
-      </h3>
-    </div>
   );
 }
